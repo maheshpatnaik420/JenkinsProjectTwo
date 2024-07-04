6de181044dc2e8d40d882e36dce2feedc278d379
mahesh patnaik <mahesh.patnaik6g@gmail.com>
2024-07-04 16:41:44 +0530

    local project

---
+++ src/main/java/com/mahesh/JenkinsProjecttwo/JenkinsProjecttwoApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.JenkinsProjecttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjecttwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjecttwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahesh/JenkinsProjecttwo/JenkinsProjecttwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.JenkinsProjecttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjecttwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
